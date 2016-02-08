ff5b13683053ce2af987789082716ba5407e9c3d
Ray Di Ciaccio <ray@appcues.com>
2016-02-08 15:04:12 -0500

    Add tests for event listeners.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
     var isCI = process.env['CI'];
 
     var baseConfig = {
-        frameworks: ['mocha', 'chai'],
+        frameworks: ['mocha', 'sinon', 'chai'],
 
         files: [
             'tests/index_test.js'
